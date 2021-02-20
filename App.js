21dc867af0853ce29b140e0c24a49d534ac55794
Donald Chung <dchung679@gmail.com>
2021-02-20 13:10:40 -0800

    Initial commit

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React , { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import firebaseApp from './firebase.js';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
 
 export default function App() {
-  return (
+    const [name, setName] = useState("");
+
+    const firestore = firebase.firestore(); 
+    useEffect(() => {
+        async function fetchUser() {
+            const ref = firestore.collection("users").doc("DonaldChung");
+            const doc = await ref.get();
+            const data = doc.data();
+            setName(data.Name);
+        }
+
+        fetchUser();
+    });
+      return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{name}</Text>
+        <Text>palceholder text</Text>
       <StatusBar style="auto" />
     </View>
   );
