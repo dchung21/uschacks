4042723103eaada8dc480f6b3089249cf55ba796
Donald Chung <dchung679@gmail.com>
2021-02-21 00:37:19 -0800

    saerch

--- SearchScreen.js
+++ SearchScreen.js
@@ -2,17 +2,45 @@ import React , { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { SearchBar } from 'react-native-elements';
+import SearchTemplate from './SearchTemplate.js';
 
+import firebaseApp from './firebase.js';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
-export default function App(props){
+export default function SearchScreen(props){
+    const [text, setText] = useState(""); 
+    const [data, setData] = useState([]);
+    const [filteredData, setFilteredData] = useState([]);
+    const [searchRes, setSearchRes] = useState([]);
+    const firestore = firebase.firestore();
 
-   const [user, setUser] = useState(""); 
+    useEffect(() => {
+        async function getUsers() {
+            const ref = firestore.collection("listUsers").doc("users");
+            const doc = await ref.get();
+            const obj = doc.data();
+            setData(Object.keys(obj));
+        }
 
-  const updateSearch = (search) => {
-      setState(search);
-      console.log(search);
-  };
+        getUsers();
+    });
 
+    const updateSearch = (search) => {
+        if (search) {
+            const startsWith = data.filter((name) => name.toLowerCase().startsWith(search.toLowerCase()));    
+            let res = [];
+            for (let i = 0; i < startsWith.length; i++) {
+                res.push(<SearchTemplate user = {startsWith[i]} />);
+            }
+
+            if (res.length == 0)
+                res.push(<Text> No users found :( </Text>);
+            setSearchRes(res);
+            setFilteredData(startsWith);
+        }
+       setText(search);
+    };
 
     return (
         <View style = {styles.container}>
@@ -23,15 +51,19 @@ export default function App(props){
           showCancel
         placeholder="Type Here..."
         onChangeText={updateSearch}
-        value={user}
+        value={text}
           containerStyle={{
             backgroundColor: '#fff',
             justifyContent: 'space-around',
             borderTopWidth:0,
             borderBottomWidth:0,
+            paddingBottom: 20,
 }}
       />
         </View>
+            <View styles = {styles.results}>
+                {searchRes}
+            </View>
         </View>
     );
   }
@@ -43,6 +75,9 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
+    },
+    results: {
+        paddingLeft: 20,
     }
 });
 
