db677213dbe37411008eb848466259f0e5c6d722
Donald Chung <dchung679@gmail.com>
2021-02-21 01:00:46 -0800

    colors

--- CardComponent.js
+++ CardComponent.js
@@ -4,8 +4,19 @@ import { Text, View, Image, StyleSheet} from "react-native";
 
 
 export default function CardComponent(props) {
+    let color;
+
+    if (props.value <= 30)
+        color = "#ffb4b4";
+    else if (props.value <= 60)
+        color = "#f5da94";
+    else if (props.valuie <= 80)
+        color = "#9fe1a2";
+    else
+        color = "#12aa48";
+
   return (
-      <Card containerStyle={{width: 300, borderRadius: 20, backgroundColor: '#ffb4b4'}} wrapperStyle={{}} onClick = {props.onClick}  >
+      <Card containerStyle={{width: 300, borderRadius: 20, backgroundColor: color}} wrapperStyle={{}} onClick = {props.onClick}  >
       <Card.Title style = {styles.titleText} >{props.title}</Card.Title>
       
       <View
